f3720af5308a72b8a6bccfcc0deb5fa896370fd0
Maksym Zaitsev <cryptolok@protonmail.com>
2018-01-31 10:04:48 +0100

    MAC entropy and stealthing impovement

--- GhostInTheNet.sh
+++ GhostInTheNet.sh
@@ -93,8 +93,9 @@ then
 		echo
 		exit 3
 	fi
-	MAC=$(echo $RANDOM|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/64:\1:\2:\3:\4:\5/')
-# random MAC, but with 0x64 in the beginning to avoid reserved addresses (unicast, etc)
+#	MAC=$(echo $RANDOM|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/64:\1:\2:\3:\4:\5/')
+	MAC="480fcf"$(head -c 6 /proc/sys/kernel/random/uuid)
+# random HP vendor MAC, to avoid reserved addresses (unicast, etc) and MAC blacklisting due to lack of randomness
 #TODO add vendors choice (dell,hp,intel,vmware,cisco,belkin...) ?
     if [[ $CMD =~ .*ifconfig ]]; then
 	    $CMD $INTERFACE hw ether $MAC
