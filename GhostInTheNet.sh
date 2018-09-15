a5374e4b2b1990b8b5119fdf91e3c3fe547949d0
Maksym Zaitsev <cryptolok@protonmail.com>
2018-09-16 00:41:34 +0200

    previous IP debug done

--- GhostInTheNet.sh
+++ GhostInTheNet.sh
@@ -142,15 +142,14 @@ then
 	then
 		dhclient $INTERFACE &> /dev/null
 	fi
-	OS=$(uname -v)
-	if [ $CMD =~ .*ip ] && [ "$OS" =~ .*Debian ]
+	if [[ $CMD =~ .*ip ]]
 	then
 		echo 'Erasing previous IP...'
 		sleep 3
 		$CMD addr del $(ip addr show dev $INTERFACE | grep inet | head -n 1 | cut -d ' ' -f 6 | cut -d '/' -f 1) dev $INTERFACE
 	fi
 # TODO use already achived IP configuration to avoid broadcast ?
-# TODO investigate previous IP for OSes
+# TODO investigate previous IP for different OSes or just ip cmd
 	echo 'Now you are a cyberspy, robotic guy'
 	echo
 #;;off)
